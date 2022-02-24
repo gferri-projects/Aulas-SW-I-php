f21f63baad1d0dc759e08b71a304e86c3808144b
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-02-24 08:23:14 -0300

    Exemplo de PHP com HTML

---
+++ aula_24_02/teste02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <title>Teste 02</title>
+</head>
+<body>
+    <h1>Teste página PHP com HTML</h1>
+
+    <p>
+        <?php echo "<p class='oi'>Olá mundo!</p>";?>
+    </p>
+
+    
+</body>
+</html>
