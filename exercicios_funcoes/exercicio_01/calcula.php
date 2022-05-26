314ddcd812428dd037103e8c600a6963b887f260
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-05-26 09:08:25 -0300

    Aula - 26/05

---
+++ exercicios_funcoes/exercicio_01/calcula.php
@@ -0,0 +1,16 @@
+<?php
+    // checkbox do usuário
+    $g_celsius = $_POST['celsius'];
+    $g_faren = $_POST['faren'];
+
+    // temperatura
+    $temperatura = $_POST['temp'];
+
+    // função para converter farenheit para celsius
+    function faren()
+
+
+
+
+
+?>
