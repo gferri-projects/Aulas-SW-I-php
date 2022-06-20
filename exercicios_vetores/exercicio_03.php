2ac731395c87767e5e7739af0bb8ee0d227c980b
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-06-20 09:05:01 -0300

    Update exercicio_03.php

--- exercicios_vetores/exercicio_03.php
+++ exercicios_vetores/exercicio_03.php
@@ -2,5 +2,19 @@
     //Crie um vetor que receba e calcule a média de 10 números gerados aleatoriamente
     //no intervalo entre 1 e 100.
 
+    $vetor = [];
+    $soma = 0;
+    for($i=0;$i<10;$i++){
+        $vetor[$i] = rand(1,100);
+        $i2 = $i+1;
+        echo "Valor $i2 = $vetor[$i]<br>";
+        $soma += $vetor[$i];
+    }
+    echo "<br>";
+    $media = $soma/10;
+    echo "A soma é: $soma<br>";
+    echo "A média é: $media"
+
+
 
 ?>
